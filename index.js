e9b7587a16a84a2a1835283e1ab5ecf767252cfc
chiranth070 <chiranth10b07@gmail.com>
2025-06-09 23:39:34 +0530

    deployment

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(express.json());
 app.use("/user", userRoutes);
 app.use("/gadget", gadgetRoutes);
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the Gadget Management API" });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
